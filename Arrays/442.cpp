f4f16b306599a586d8eb8af53ad783ddd61f89f3
Vaishali Singh <140580678+vaishaliisingh@users.noreply.github.com>
2024-08-31 21:46:44 +0530

    Create 442.cpp

---
+++ Arrays/442.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector <int> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(i+1>=nums.size()) break;
+            if(nums[i]==nums[i+1]){
+                ans.push_back(nums[i]);
+                i++;
+            }
+        }
+        return ans;
+    }
+};
